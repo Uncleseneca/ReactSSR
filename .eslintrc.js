f964fecf934d91f403144c49ad6e3a093f6e4f61
Vladimir Golotin <uncleseneca@gmail.com>
2018-02-17 14:04:59 +0300

    added redux

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,3 @@
 module.exports = {
-  extends: 'airbnb',
-  rules: {
-    'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx'] }],
-    'react/prop-types': [0],
-  },
+    "extends": "airbnb"
 };
