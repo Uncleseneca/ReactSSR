9a090da412bf24b4cd4f72f3a0c01ed4eef1a50c
Vladimir Golotin <uncleseneca@gmail.com>
2018-02-18 13:28:01 +0300

    added cookie handling

--- src/client/client.js
+++ src/client/client.js
@@ -4,13 +4,22 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
+import axios from 'axios';
 import { Provider } from 'react-redux';
 import { renderRoutes } from 'react-router-config';
 
 import rootReducer from './reducers';
 import Routes from './Routes';
 
-const store = createStore(rootReducer, window.INITIAL_STATE, applyMiddleware(thunk)); // eslint-disable-line no-undef
+const axiosInstance = axios.create({
+  baseURL: '/api',
+});
+
+const store = createStore(
+  rootReducer,
+  window.INITIAL_STATE,
+  applyMiddleware(thunk.withExtraArgument(axiosInstance)),
+);
 
 ReactDOM.hydrate(
   <Provider store={store}>
